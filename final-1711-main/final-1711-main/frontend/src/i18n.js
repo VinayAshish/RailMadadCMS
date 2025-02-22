a0a33c530ec74f51ce31a920b7edeb6cec4e7f95
Anandam Vinay Ashish <145482199+VinayAshish@users.noreply.github.com>
2025-02-22 10:03:47 +0530

    Initial commit: Added RailMadaCMS project files
    all the files of the project

---
+++ final-1711-main/final-1711-main/frontend/src/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+// Import translation files
+import enTranslation from './en.json';
+import hiTranslation from './hi.json';
+import knTranslation from './kn.json';
+import teTranslation from './tel.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      hi: { translation: hiTranslation },
+      kn: { translation: knTranslation },
+      tel: { translation: teTranslation },
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
